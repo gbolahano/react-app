479f4716c843103a26b90d03ae76e6989b3c14d2
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2022-04-19 10:05:41 +0100

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: 'master',
+  repositoryUrl: 'https://github.com/gbolahanO/react-app',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
